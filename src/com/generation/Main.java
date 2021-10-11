f96b4a2064b2ace55c5b1a2eac6a1447632ae9ee
Sheila Sanchez Ramirez <sanchezramirezsheila@gmail.com>
2021-10-11 17:39:38 -0500

    Ejercicio de prueba de operadores lógicos

---
+++ src/com/generation/Main.java
@@ -0,0 +1,19 @@
+package com.generation;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        boolean x=true;
+        boolean y=false;
+        boolean z=true;
+
+        System.out.println("a) "+ (x && y||x && z) );
+        System.out.println("b) "+((x || !y) && (!x | z)) );
+        System.out.println("c) "+( x || y && z));
+        System.out.println("d) "+(!(x || y) && z));
+        System.out.println("e) "+( x || y || x && !z && !y));
+        System.out.println("f) "+( !x || !y || z && x && !y));
+
+    }
+}
